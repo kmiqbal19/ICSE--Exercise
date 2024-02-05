43cbad608376dfe52cec188e65d27238c8edf436
kmiqbal19 <km.iqbal01@gmail.com>
2024-02-05 23:58:55 +0100

    practice 2

--- Practice_exam/practice.py
+++ Practice_exam/practice.py
@@ -1,3 +1,6 @@
+# BINARY SEARCH WITH RECURSION
+
+
 def binary_search (array: list[int] , find_value: int):
     def _find(a: list[int], x: int, left: int, right: int):
         if left > right:
@@ -12,7 +15,7 @@ def binary_search (array: list[int] , find_value: int):
         
     return _find(array, find_value, 0, len(array) - 1)
 
-
+# BINARY SEARCH WITH ITERATION
 def binary_search_iteration (array: list[int] , find_value: int):
     left, right = 0 , len(array) - 1
     while left <= right:
@@ -27,3 +30,63 @@ def binary_search_iteration (array: list[int] , find_value: int):
 
 print(binary_search([2,5,8,9,36,45,89,98] , 36))
 print(binary_search_iteration([2,5,8,9,36,45,89,98] , 89))
+
+
+
+# IMPLEMENT LINKED_LIST DATA STRUCTURE
+
+
+class Node:
+    def __init__(self, data: str = None):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data:str):
+        if not self.head:
+            self.head = Node(data)
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = Node(data)
+        return
+    def prepend(self, data:str):
+        new_head = Node(data)
+        new_head.next = self.head
+        self.head = new_head
+    def delete(self, data: str):
+        if not self.head:
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        current =self.head
+        while current.next:
+            if current.next.data == data:
+                current.next = current.next.next
+                return
+            current = current.next
+        return
+    def display(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        print(elements)
+        return elements
+    
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(5)
+ll.append(89)
+ll.append(1000)
+ll.append(158165)
+ll.delete(5)
+ll.prepend(888888)
+ll.display()
